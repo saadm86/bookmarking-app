5845cc8d27320d049b4fc89caa0fd6abd37d0639
Saad Muhamamd <s.muhammad86@gmail.com>
2021-01-13 22:26:20 +0500

    finalized the functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,73 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useEffect, useState } from "react"
+import { useQuery, useMutation } from "@apollo/client"
+import gql from "graphql-tag"
+import './styles.css'
+import Card from '../components/Card'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const GET_DATA = gql`
+  {
+    bookmarks {
+      title
+      url
+    }
+  }
+`
+const ADD_BOOKMARK = gql`
+    mutation addBookmark($title: String!, $url: String!){
+        addBookmark(title: $title, url: $url){
+            id
+        }
+    }
+`
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  let titleField
+  let urlField
+  const [addBookmark] = useMutation(ADD_BOOKMARK)
+  const { data, loading, error } = useQuery(GET_DATA)
+  if (loading) return <h1>{loading}</h1>
+  if (error) return <h1>{error}</h1>
+
+  const handleSubmit = () => {
+    addBookmark({
+        variables: {
+            title: titleField.value,
+            url: urlField.value
+        },
+        refetchQueries: [{ query: GET_DATA }]
+    })
+}
+  return (
+    <div className="container">
+      <h2>Add New Bookmark</h2>
+      <label>
+        Bookmark Title <br />
+        <input type="text" ref={node => titleField = node} />
+      </label>
+      <br />
+      <label>
+        Bookmark URL <br />
+        <input type="text" ref={node => (urlField = node)} />
+      </label>
+
+      <br />
+      <br />
+      <button onClick={handleSubmit}>Add Bookmark</button>
+    
+
+    <h2>My Bookmark List</h2>
+    {/* {JSON.stringify(data.bookmarks)} */}
+
+    <div className="card-container">
+        {data.bookmarks.map((bm) => <Card url={bm.url} title={bm.title} />)}
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+
+    </div>
+
+
+
+
+  )
+}
 
 export default IndexPage
